c6ad007c2bda43ed5b80446a756db31d42ca7495
Janos Czentye <czentye@tmit.bme.hu>
2016-05-05 10:05:39 +0200

    Finish generator func for /ovsflows REST call

--- webserver.py
+++ webserver.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import json
 import subprocess
 import sys
 
@@ -29,7 +30,8 @@ def api_ovsports ():
 def api_ovsflows ():
   res = subprocess.check_output(
     ["sudo", OVS_DIR + "ovs-ofctl", "dump-flows", DBR])
-  return res
+  flows = list(__flow_processor(res))
+  return json.dumps(flows), 200, {'Content-Type': 'text/application/json'}
 
 
 @app.route('/running')
@@ -39,17 +41,33 @@ def api_running ():
 
 
 @app.route('/start', methods=['GET'])
-def api_start():
-  cmask=request.args['cmask']
-  mem=request.args['mem']
-  subprocess.call(["sudo", OVS_DIR + "ovs-vsctl", "add-port", DBR ,"vhost1" ,"--" ,"set" ,"Interface" ,"vhost1" ,"type=dpdkvhostuser"])
-  subprocess.call(["sudo", OVS_DIR + "ovs-ofctl" , "add-flow", DBR, "in_port=1,actions=output:3"])
-  subprocess.call(["sudo", OVS_DIR + "ovs-ofctl" , "add-flow", DBR, "in_port=3,actions=output:2"])
-  pid=subprocess.Popen(["sudo", "docker" ,"run", "-d", "-v" ,"/usr/local/var/run/openvswitch/vhost1:/var/run/usvhost" ,"-v", "/dev/hugepages:/dev/hugepages", "dpdk-l2fwd", "./examples/l2fwd/build/l2fwd", "-c", cmask, "-n", "4", "-m", mem, "--no-pci", "--single-file", "--file-prefix", "fw", "--vdev=eth_cvio0,mac=00:01:02:03:04:05,path=/var/run/usvhost", "--", "-p", "0x1"]).pid
+def api_start ():
+  cmask = request.args['cmask']
+  mem = request.args['mem']
+  subprocess.call(["sudo", OVS_DIR + "ovs-vsctl", "add-port", DBR,
+                   "vhost1", "--", "set", "Interface", "vhost1",
+                   "type=dpdkvhostuser"])
+  subprocess.call(["sudo", OVS_DIR + "ovs-ofctl", "add-flow", DBR,
+                   "in_port=1,actions=output:3"])
+  subprocess.call(["sudo", OVS_DIR + "ovs-ofctl", "add-flow", DBR,
+                   "in_port=3,actions=output:2"])
+  pid = subprocess.Popen(
+    ["sudo", "docker", "run", "-d",
+     "-v", "/usr/local/var/run/openvswitch/vhost1:/var/run/usvhost",
+     "-v", "/dev/hugepages:/dev/hugepages",
+     "dpdk-l2fwd", "./examples/l2fwd/build/l2fwd",
+     "-c", cmask, "-n", "4", "-m", mem, "--no-pci",
+     "--single-file", "--file-prefix", "fw",
+     "--vdev=eth_cvio0,mac=00:01:02:03:04:05,path=/var/run/usvhost",
+     "--", "-p", "0x1"]).pid
   return str(pid)
 
-#$sudo docker run -d -v /usr/local/var/run/openvswitch/vhost1:/var/run/usvhost -v /dev/hugepages:/dev/hugepages dpdk-test
-#./examples/l2fwd/build/l2fwd -c 0x4 -n 4 -m 1024 --no-pci --single-file --file-prefix fw --vdev=eth_cvio0,mac=00:01:02:03:04:05,path=/var/run/usvhost -- -p 0x1
+
+# $sudo docker run -d -v /usr/local/var/run/openvswitch/vhost1:/var/run
+# /usvhost -v /dev/hugepages:/dev/hugepages dpdk-test
+# ./examples/l2fwd/build/l2fwd -c 0x4 -n 4 -m 1024 --no-pci --single-file
+# --file-prefix fw --vdev=eth_cvio0,mac=00:01:02:03:04:05,
+# path=/var/run/usvhost -- -p 0x1
 
 
 @app.route('/stop/<cid>')
@@ -59,5 +77,22 @@ def api_stop (cid):
   subprocess.call(["sudo", OVS_DIR + "ovs-ofctl", "del-flows", DBR])
   return ret
 
+
+def __flow_processor (raw):
+  lines = iter(raw.strip().split('\n'))
+  lines.next()
+  for line in lines:
+    print 'Line:', line
+    flow = {}
+    for fragment in line.strip().split(' '):
+      if fragment.startswith(('cookie', 'duration', 'table', 'n_packets',
+                              'n_bytes', 'idle_age', 'actions')):
+        field, value = fragment.strip(',').split('=')
+        flow[field] = value
+      elif fragment:
+        flow['match'] = fragment
+    yield flow
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+  app.run(host='0.0.0.0', port=5000)
